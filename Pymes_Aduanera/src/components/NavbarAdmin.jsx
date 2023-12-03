fe211ce1765072c224d3ba516e46876c0e998d85
David Torres <angeldtm09@gmail.com>
2023-12-03 00:19:14 -0600

    Changes Css Navbar

--- Pymes_Aduanera/src/components/NavbarAdmin.jsx
+++ Pymes_Aduanera/src/components/NavbarAdmin.jsx
@@ -2,16 +2,37 @@ import '../css/admin/Navbar.css';
 
 const NavbarAdmin = () => {
   return (
-    <nav className="navbar">
-      <div className="navbar-brand">Pymes Aduanera</div>
-      <ul className="navbar-nav">
-        <li className="nav-item">Clientes</li>
-        <li className="nav-item">Acerca de</li>
-        <li className="nav-item">Contacto</li>
-        <li className="nav-item">Profile</li>
-        <li className="nav-item">Exit</li>
-      </ul>
-    </nav>
+    <>
+     <div className="navbar-container">
+      <nav className="navbar">
+        <img src="https://cdn-icons-png.flaticon.com/512/1996/1996005.png" alt="Logo de la empresa" className="logo" />
+        <div className="navbar-brand">Pymes Aduanera</div>
+        <ul className="navbar-nav">
+          {/* Registrar clientes, ver  */}
+          <li className="nav-item">Clientes</li> 
+          {/* Soporte clientes */}
+          <li className="nav-item">Tickets</li> 
+          {/* Paqueteria */}
+          <li className="nav-item">Paqueteria</li>
+          {/* Cotizaciones */}
+          <li className="nav-item">Cotizaciones</li>
+          {/* Importaciones/Exportaciones */}
+          <li className="nav-item">Imp./Exp.</li>
+          {/* Revision */}
+          <li className="nav-item">Revision</li>
+          {/* Seguimiento */}
+          <li className="nav-item">Mi tracking</li>
+          {/* Perfil */}
+          <li className="nav-item">Profile</li>
+          {/* Salir */}
+          <li className="nav-item">Salir</li>
+        </ul>
+      </nav>
+    </div>
+    <h1 className='phrase'>Explora sin límites. Conectamos caminos, creamos oportunidades. Bienvenido a nuestra aduana, donde los sueños toman vuelo.
+    </h1>
+    </>
+   
   );
 };
 
--- Pymes_Aduanera/src/pages/admin/IndexAdmin.jsx
+++ Pymes_Aduanera/src/pages/admin/IndexAdmin.jsx
@@ -1,15 +1,13 @@
 import NavbarAdmin from '../../components/NavbarAdmin';
 
-
 function IndexAdmin () {
 
     return (
-        <>
+        <div className='containerIndexAdmin'>
             <NavbarAdmin></NavbarAdmin>
-            <h1 className='phrase'>Explora sin límites. Conectamos caminos, creamos oportunidades. Bienvenido a nuestra aduana, donde los sueños toman vuelo.
-            </h1>
-            {/* <ProfileForm></ProfileForm> */}
-        </>
+            
+            
+        </div>
     );
 }
 export default IndexAdmin;
