54b328e460d290001088cb4e02029e0ddb6c479d
David Torres <angeldtm09@gmail.com>
2023-12-03 23:14:21 -0600

    Parcel finished

--- Pymes_Aduanera/src/components/ParcelManager.jsx
+++ Pymes_Aduanera/src/components/ParcelManager.jsx
@@ -1,30 +1,62 @@
 import { useState } from 'react';
 import "../css/admin/ParcelManager.css"
+import File from "../Files/Parsersmeaning.pdf"
 
 const PaquetesManager = () => {
   const [mostrarFormulario, setMostrarFormulario] = useState(false);
   const [paquetes, setPaquetes] = useState([
     {
-        tipoProducto: 'Electronics',
-        origen: 'New York, USA',
-        direccionDestino: '123 Main St, Cityville',
-        numeroCasillero: 'C12345',
-        numeroTracking: '123456789',
-      },
-      {
-        tipoProducto: 'Clothing',
-        origen: 'Paris, France',
-        direccionDestino: '456 Fashion Ave, Style City',
+        tipoProducto: 'Beauty Products',
+        origen: 'Los Angeles, USA',
+        direccionDestino: '456 Glam St, Beauty City',
         numeroCasillero: 'C67890',
         numeroTracking: '987654321',
+        detalleTracking: 'En proceso de empaquetado',
+        Estado: 'En tránsito',
+        Archivo: 'beauty_product_invoice.pdf', // Nombre del documento
       },
       {
-        tipoProducto: 'Books',
-        origen: 'London, UK',
-        direccionDestino: '789 Library St, Bookland',
+        tipoProducto: 'Sportswear',
+        origen: 'Barcelona, Spain',
+        direccionDestino: '789 Active Ave, Fitness Town',
         numeroCasillero: 'C24680',
         numeroTracking: '555111333',
+        detalleTracking: 'En clasificación aduanera',
+        Estado: 'En espera',
+        Archivo: 'sportswear_order.docx', // Nombre del documento
       },
+      {
+        tipoProducto: 'Educational Books',
+        origen: 'Tokyo, Japan',
+        direccionDestino: '101 Learning St, Knowledge City',
+        numeroCasillero: 'C13579',
+        numeroTracking: '876543210',
+        detalleTracking: 'En proceso de entrega',
+        Estado: 'En ruta',
+        Archivo: 'educational_books_manifest.xlsx', // Nombre del documento
+      },
+      {
+        tipoProducto: 'Toys',
+        origen: 'Sydney, Australia',
+        direccionDestino: '321 Playful Blvd, Toyland',
+        numeroCasillero: 'C24601',
+        numeroTracking: '1122334455',
+        detalleTracking: 'En revisión de seguridad',
+        Estado: 'En espera de inspección',
+        Archivo: 'toys_safety_certificate.pdf', // Nombre del documento
+      },
+      {
+        tipoProducto: 'Sports Equipment',
+        origen: 'Rio de Janeiro, Brazil',
+        direccionDestino: '555 Stadium Rd, Sports City',
+        numeroCasillero: 'C78901',
+        numeroTracking: '5432109876',
+        detalleTracking: 'En proceso de envío',
+        Estado: 'En camino',
+        Archivo: 'sports_equipment_warranty.doc', // Nombre del documento
+      }
+
+      
   ]);
   const [paqueteSeleccionado, setPaqueteSeleccionado] = useState({
     tipoProducto: '',
@@ -32,66 +64,74 @@ const PaquetesManager = () => {
     direccionDestino: '',
     numeroCasillero: '',
     numeroTracking: '',
+    detalleTracking: '',
+    Estado: '',
+    Archivo: ''
   });
   
   
   const [editIndex, setEditIndex] = useState(null);
 
   const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setPaqueteSeleccionado((prevPaquete) => ({
-      ...prevPaquete,
-      [name]: value,
-    }));
-  };
-
-  const agregarPaquete = () => {
-    if (editIndex !== null) {
-      // If editIndex is not null, it means we are editing an existing package
-      const nuevosPaquetes = [...paquetes];
-      nuevosPaquetes[editIndex] = { ...paqueteSeleccionado };
-      setPaquetes(nuevosPaquetes);
-
-      // Reset the editIndex after saving the edition
-      setEditIndex(null);
+    const { name, value, type } = e.target;
+  
+    if (type === 'file') {
+      const file = e.target.files[0];
+      setPaqueteSeleccionado((prevPaquete) => ({
+        ...prevPaquete,
+        [name]: file,
+      }));
     } else {
-      // If editIndex is null, it means we are adding a new package
-      setPaquetes((prevPaquetes) => [...prevPaquetes, { ...paqueteSeleccionado }]);
+      setPaqueteSeleccionado((prevPaquete) => ({
+        ...prevPaquete,
+        [name]: value,
+      }));
     }
-
-    // Clear the form
-    resetForm();
   };
+  
+
+  const agregarPaquete = async () => {
+    if (paqueteSeleccionado.Archivo) {
+      const formData = new FormData();
+      formData.append('Archivo', paqueteSeleccionado.Archivo);
+      try {
+        const response = await fetch('/ruta-de-tu-api-para-cargar-archivos', {
+          method: 'POST',
+          body: formData,
+        });
+  
+      } catch (error) {
+        console.error('Error al cargar el archivo:', error);
+      }
+    }
+  
+    // Resto del código...
+  };
+  
 
   const editarPaquete = (index) => {
-    // Set the selected package for editing
     setPaqueteSeleccionado({ ...paquetes[index] });
     setEditIndex(index);
-
-    // Show the modal
     setMostrarFormulario(true);
   };
 
   const eliminarPaquete = (index) => {
-    // Implement the logic to delete the selected package
     const nuevosPaquetes = paquetes.filter((_, i) => i !== index);
     setPaquetes(nuevosPaquetes);
   };
 
   const resetForm = () => {
-    // Clear the form
     setPaqueteSeleccionado({
       tipoProducto: '',
       origen: '',
       direccionDestino: '',
       numeroCasillero: '',
       numeroTracking: '',
+      detalleTracking: '',
+      Estado: '',
+      Archivo: ''
     });
-
-    // Reset the editIndex
     setEditIndex(null);
-
-    // Hide the modal
     setMostrarFormulario(false);
   };
 
@@ -101,6 +141,7 @@ const PaquetesManager = () => {
 
   return (
     <div className="paquetes-manager">
+      <h1 className='pm-title'>Agregar Nuevo Paquete</h1>
       <button className="btn-agregar" onClick={() => setMostrarFormulario(true)}>
         Agregar Paquete
       </button>
@@ -155,6 +196,33 @@ const PaquetesManager = () => {
                     onChange={handleInputChange}
                     />
                 </label>
+                <label>
+                    Detalle Tracking:
+                    <input
+                    type="text"
+                    name="detalleTracking"
+                    value={paqueteSeleccionado.detalleTracking}
+                    onChange={handleInputChange}
+                    />
+                </label>
+                <label>
+                    Estado:
+                    <input
+                    type="text"
+                    name="detalleTracking"
+                    value={paqueteSeleccionado.Estado}
+                    onChange={handleInputChange}
+                    />
+                </label>
+                <label className="formLabel">
+                Archivo
+                <input
+                    type="file"
+                    name="Archivo"
+                    onChange={handleInputChange}
+                    className='fileParcel'
+                />
+                </label>
                 <button
                 className="pm-btn-guardar"
                 type="button"
@@ -178,6 +246,9 @@ const PaquetesManager = () => {
                     <th>Destino</th>
                     <th>Casillero</th>
                     <th>Tracking</th>
+                    <th>Detalle Tracking</th>
+                    <th>Estado</th>
+                    <th>Archivo</th>
                     <th>Acciones</th>
                 </tr>
             </thead>
@@ -189,9 +260,12 @@ const PaquetesManager = () => {
                     <td>{paquete.direccionDestino}</td>
                     <td>{paquete.numeroCasillero}</td>
                     <td>{paquete.numeroTracking}</td>
+                    <td>{paquete.detalleTracking}</td>
+                    <td>{paquete.Estado}</td>
+                    <td><a download="" href={File} className="pm-button-link">Download</a></td>
                     <td>
-                        <button onClick={() => editarPaquete(index)}>Editar</button>
-                        <button onClick={() => eliminarPaquete(index)}>Eliminar</button>
+                        <button onClick={() => editarPaquete(index)} className="pm-button">Editar</button>
+                        <button onClick={() => eliminarPaquete(index)} className="pm-button">Eliminar</button>
                     </td>
                 </tr>
                 ))}
